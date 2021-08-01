117912e7e388be6ba0278da34fcb2cf3c0a6414c
U Sai Nath Rao <87133044+usai729@users.noreply.github.com>
2021-08-01 19:32:42 +0530

    allow.php

--- allow.php
+++ allow.php
@@ -4,11 +4,13 @@
 
     $pendingID = $_POST['hiddenInputID'];
 
+    //MySQL Query 
     $sql_allow = "INSERT INTO students(stuPassword, stuName, stuPhNo, stuClass)
                 SELECT stuPassword, stuName, stuPhNo, stuClass FROM pendingStudents WHERE PstuId='$pendingID' LIMIT 1
             ";
 
     if (mysqli_query($conn, $sql_allow)) {
+        //Delete students from pendingStudents table after inserting him/her in students table
         $sql_delete = "DELETE FROM pendingStudents WHERE PstuId='$pendingID'"; 
 
         if (mysqli_query($conn, $sql_delete)) {
